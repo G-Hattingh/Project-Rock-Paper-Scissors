0f7c80ebb1d6f403d6724d78017aefd3dc7dd449
G-Hattingh <160169540+G-Hattingh@users.noreply.github.com>
2025-09-03 13:39:10 -0500

    Add Conditional Returns

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
 function getComputerChoice () {
     let x = Math.floor((Math.random) * 3);
+    if (x === 0) {
+        return "Computer chose Rock";
+    } else if  (x ===1) {
+        return "Computer chose Rock";
+    } else {
+        return "Computer chose Scissors";
+    }
 }
