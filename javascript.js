8658a58596b9f7e26986a34c1e8924bc2cac092a
G-Hattingh <160169540+G-Hattingh@users.noreply.github.com>
2026-02-19 20:32:44 -0600

    Fix error

--- javascript.js
+++ javascript.js
@@ -58,7 +58,7 @@ function playGame() {
          console.log("You lose: Try Again");
         }
 
- return "Final Score:  You: " + humanScore + " / Computer: " + computerScore;
+ console.log("Final Score:  You: " + humanScore + " / Computer: " + computerScore); 
 
 };
 playGame();
