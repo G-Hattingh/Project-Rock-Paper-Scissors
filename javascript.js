1ed54bcc6700a352c10cb86b372de3fa4751591c
G-Hattingh <160169540+G-Hattingh@users.noreply.github.com>
2026-02-19 20:17:19 -0600

    Add Ending Wording
    Clearer scores at the end of game

--- javascript.js
+++ javascript.js
@@ -20,24 +20,29 @@ function playGame() {
         const computerScore = 0;
         const humanScore = 0;
 
-        function playRound(computerChoice, humanChoice) {
-        computerChoice = getComputerChoice();
-        // humanChoice is now case-insensitive 
-        humanChoice = humanChoice.toLowerCase();
+     function playRound(computerChoice, humanChoice) {
+                computerChoice = getComputerChoice();
+                // humanChoice is now case-insensitive 
+                humanChoice = humanChoice.toLowerCase();
 
-        if (humanChoice === computerChoice) {
-            console.log("You Tied: Play Again");
-        }
-        else if(humanChoice === "rock" && computerChoice === "scissors" ||
-                humanChoice === "paper" && computerChoice === "rock"    ||
-                humanChoice === "scissors" && computerChoice === "paper" ) {
-            console.log("You Won: Congratulations");
-                }
-        else {
-            console.log("You lose: Try Again");
-        }
+             if (humanChoice === computerChoice) {
+                    console.log("You Tied: Play Again"); }
 
-}
+            else if(humanChoice === "rock" && computerChoice ===      "scissors"                                            ||
+                     humanChoice === "paper" && computerChoice === "rock"                                        ||
+                     humanChoice === "scissors" && computerChoice === "paper" ) {
+                    humanScore++;
+                    console.log("You Won: Congratulations");}
+
+            else {
+                    computerScore++;
+                    console.log("You lose: Try Again");}}
+
+                return "Final Score:  You: " + humanScore + " / Computer: " + computerScore;
+
+                
+
+
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
